2d88c1260d09ab0825e5d8367e8a07a3bbb1ef42
YY-Cherry <YY@YYMac.local>
2019-12-06 22:17:03 +0800

    feat: layout

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Hello from './components/Hello';
+import LayoutFlex from './components/layoutFlex';
+// import Hello from './components/Hello';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello name="TypeScript" />, document.getElementById('root'));
+ReactDOM.render(
+  <LayoutFlex />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
