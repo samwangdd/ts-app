44823bfa13e712fc3d7a266f46860fe88fbf86ff
sam <wyysammore@gmail.com>
2020-03-25 23:19:14 +0800

    feat(tool): type

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -2,7 +2,7 @@
  * 防抖
  * @param {function} fn
  */
-function deBounce(fn, delay) {
+export function deBounce(fn, delay) {
   let timeOut = null;
   console.log('deBounce :', this);
   return function() {
@@ -44,3 +44,25 @@ export function DeepClone(obj, hash = new WeakMap()) {
   }
   return cloneCon;
 }
+
+/**
+ * 判断传入对象的数据类型
+ * @param {*} params
+ */
+function type(obj) {
+  if (obj === null) return String(obj);
+  const exception = ['Array', 'Regxp', 'Date', 'Object', 'Error'];
+  const class2type = {};
+  exception.forEach(i => (class2type[`[object ${i}]`] = i.toLowerCase()));
+  return typeof obj === 'object'
+    ? class2type[Object.prototype.toString.call(obj)] || 'object'
+    : typeof obj;
+}
+
+// console.log('object :', type('yy&sam'));
+// console.log('object :', type(null));
+// console.log('object :', type(1123));
+// console.log('object :', type(new Date()));
+// console.log('object :', type(/\3*?$/));
+// console.log('object :', type([]));
+// console.log('object :', type({}));
